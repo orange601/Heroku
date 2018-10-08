30528e12242077eca6464c136ab0b9ccfb29b039
orange601 <oltop@naver.com>
2018-10-09 00:17:16 +0900

    sss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http'); 
 var server = http.createServer();
 server.on('request', doRequest); 
-server.listen(3000); 
+server.listen(5000); 
 console.log('Server running!'); // 요청 처리 
 function doRequest(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'}); 
