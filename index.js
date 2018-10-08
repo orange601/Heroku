8c9f4c61ee9633b3272a4cfd5519c200349bf961
orange601 <oltop@naver.com>
2018-10-09 00:33:45 +0900

    port changed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var http = require('http'); 
 var server = http.createServer();
 server.on('request', doRequest); 
-server.listen(5000); 
+server.listen(process.env.PORT || 5000);
 server.timeout = 180000;
 console.log('Server running!'); // 요청 처리 
 function doRequest(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'}); 
-	res.write('Hello Wddorld\n'); res.end(); 
+	res.write('Hello Wddor222222222ld\n'); res.end(); 
 }
