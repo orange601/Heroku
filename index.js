72c4f7fee20b73879f0eff276049dd6e682dec65
orange601 <oltop@naver.com>
2018-10-09 00:29:20 +0900

    timeout changed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var server = http.createServer();
 server.on('request', doRequest); 
 server.listen(5000); 
+server.timeout = 180000;
 console.log('Server running!'); // 요청 처리 
 function doRequest(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'}); 
