b2b5d4baa2150633ebca9b3abbc22ca58268d8ce
orange601 <oltop@naver.com>
2018-10-08 23:22:04 +0900

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express') 
 var app = express() 
 var port = process.env.PORT || 3000 
-app.get('/', function (req, res) { res.send('Hello World!') }) 
+app.get('/ss', function (req, res) { res.send('Hello World!') }) 
 app.listen(port, function () { console.log('open port 3000!') })
